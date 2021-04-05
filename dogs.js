e19eb76166f0a5b8d2376a0dbeec0ba61e537eca
Yinuo-M <mengyn1999@gmail.com>
2021-04-05 17:56:43 +0100

    Add a list of dogs

---
+++ dogs.js
@@ -0,0 +1,16 @@
+export let dogList = [
+	{
+		name: "Andy",
+		age: 5,
+		breed: "Labrador Retriever",
+		location: "Wiltshire, England",
+		doginalities: [
+			"shy and quite",
+			"easy to train",
+			"crazy about food",
+			"very smart",
+			"pro fetch player",
+			"loves cuddles",
+		],
+	},
+];
