2f33e8cf9012fcf96eeae0e9b3fa30d12e029dbf
Yinuo-M <mengyn1999@gmail.com>
2021-04-07 16:53:49 +0100

    Update list page sorting functions

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function cancelDefault(e) {
 
 //ANCHOR list page dog profiles
 //TODO make something that remember the position on the list to come back to.
-
 let displayList = [...dogList].sort((a, b) => (a.name > b.name ? 1 : -1));
+
 let listStatus = {
 	done: false,
 	index: 0,
@@ -80,6 +80,49 @@ function loadMore() {
 
 function scrollToTop() {
 	window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
+
+const sortByInput = document.getElementById("sort");
+const regionInput = document.getElementById("region");
+const minAgeInput = document.getElementById("min-age");
+const maxAgeInput = document.getElementById("max-age");
+sortByInput.addEventListener("change", sortDogs);
+regionInput.addEventListener("change", sortDogs);
+minAgeInput.addEventListener("change", sortDogs);
+maxAgeInput.addEventListener("change", sortDogs);
+
+function sortDogs() {
+	const sortBy = sortByInput.value;
+	const region = regionInput.value;
+	const minAge = minAgeInput.value;
+	const maxAge = maxAgeInput.value;
+
+	displayList = dogList.filter((dog) => {
+		const regionMatch =
+			region === "any" || dog.location.toLowerCase().includes(region);
+		if (!regionMatch) return;
+
+		if (dog.age >= minAge && dog.age <= maxAge) return true;
+	});
+
+	if (sortBy === "age-ascend") {
+		displayList.sort((a, b) => (a.age > b.age ? 1 : -1));
+	} else if (sortBy === "age-descend") {
+		displayList.sort((a, b) => (a.age < b.age ? 1 : -1));
+	}
+
+	const previewList = document.getElementById("dogs");
+	previewList.innerHTML = "";
+	resetStatus();
+	populatePage(displayList);
+}
+
+function resetStatus() {
+	listStatus = {
+		done: false,
+		index: 0,
+		itemsPerPage: 5,
+	};
 }
 
 populatePage(displayList);
