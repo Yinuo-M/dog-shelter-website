77fc7ecb3c0e902a5695645fdb3cf8619ece7f53
Yinuo-M <mengyn1999@gmail.com>
2021-04-28 13:32:51 +0100

    Improve front page responsiveness

--- common.js
+++ common.js
@@ -19,3 +19,52 @@ if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
 if (module.hot) {
 	module.hot.accept();
 }
+
+//ANCHOR hamburger menu
+const menuButton = document.querySelector(".hamburger");
+const navList = document.querySelector("#nav-list");
+menuButton.addEventListener("click", toggleMenu);
+function toggleMenu() {
+	if (navList.classList.contains("menu-open")) {
+		closeMenu();
+	} else {
+		openMenu();
+	}
+}
+
+function openMenu() {
+	navList.classList.add("transition");
+	navList.classList.add("menu-open");
+	navList.setAttribute("aria-hidden", "false");
+	navList.querySelectorAll("a").forEach((a) => (a.tabIndex = 0));
+}
+
+function closeMenu() {
+	navList.classList.remove("menu-open");
+	navList.setAttribute("aria-hidden", "true");
+	navList.querySelectorAll("a").forEach((a) => (a.tabIndex = -1));
+}
+
+window.addEventListener("resize", restoreMenu);
+function restoreMenu() {
+	restorePCMenu();
+	restorePhoneMenu();
+}
+
+function restorePCMenu() {
+	if (document.documentElement.clientWidth > 580) {
+		navList.classList.remove("menu-open");
+		navList.setAttribute("aria-hidden", "false");
+		navList.querySelectorAll("a").forEach((a) => (a.tabIndex = 0));
+		navList.classList.remove("transition");
+	}
+}
+
+function restorePhoneMenu() {
+	if (document.documentElement.clientWidth <= 580) {
+		navList.setAttribute("aria-hidden", "true");
+		navList.querySelectorAll("a").forEach((a) => (a.tabIndex = -1));
+	}
+}
+
+window.addEventListener("DOMContentLoaded", restorePhoneMenu);
